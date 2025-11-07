6a6dbe439be652adfc0043776a9557357d892b31
Bojan Antonovic <bojan@antonovic.ch>
2025-11-07 18:56:40 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ch.antonovic.microchipcomtest {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ch.antonovic.microchipcomtest to javafx.fxml;
+    exports ch.antonovic.microchipcomtest;
+}
